e8177774205bab99d7ca535e53312c28017ba19d
Piotr45 <baryczkowski.piotr@gmail.com>
2023-03-21 12:07:46 +0100

    Add prepare dataset | Add temp run script

--- af-termination/neural-network/prepare-dataset.py
+++ af-termination/neural-network/prepare-dataset.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 import sys
-from typing import List
+import math
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
 import wfdb
+from numpy.typing import ArrayLike
 
 
 def parse_arguments(argv: List[str]) -> argparse.Namespace:
@@ -56,6 +58,76 @@ def _list_all_data(dir: str) -> List[str]:
     return os.listdir(dir)
 
 
+def _list_records(dir: str) -> List[str]:
+    """List all record names for a given person
+    
+    Args:
+        dir (str): directory containing records
+    
+    Returns:
+        List[str]: record names
+    """
+    all_record_files = os.listdir(dir)
+    record_names = list(
+        set([file.split(".")[0] for file in all_record_files])
+    )
+    return record_names
+
+
+def _read_record_and_annotation(
+    data_dir: str, record_name: str
+) -> Tuple[wfdb.Record, wfdb.Annotation]:
+    """Read record's data and annotation of a given record
+    
+    Args:
+        data_dir (str): directory containing records
+        record_name (str): record name
+    
+    Returns:
+        Tuple[wfdb.Record, wfdb.Annotation]: tuple of record's data and annotation
+    """
+    if data_dir[-1] != "/":
+        data_dir += "/"
+
+    record = wfdb.rdrecord(f"{data_dir}{record_name}")
+    annotation = wfdb.rdann(f"{data_dir}{record_name}", extension="qrs")
+    label = record_name[0]
+
+    return (record, annotation, label)
+
+
+def _prepare_record_data(
+    record: wfdb.Record, annotation: wfdb.Annotation, label: str, sample_length = 64
+) -> Tuple[ArrayLike, ArrayLike]:
+    """
+    """
+    ann_points = annotation.sample
+
+    record_length = record.p_signal.shape[0]
+    num_ecgs = record.p_signal.shape[1]
+    
+    data_size = math.ceil(record_length / sample_length)
+
+    ecg_data = np.zeros((data_size, sample_length, num_ecgs), dtype=np.float32)
+    labels = np.zeros((data_size, 2), dtype=np.int8)
+
+    dummy_encoder = {
+        'n': np.array([0, 0], dtype=np.int8),
+        's': np.array([0, 1], dtype=np.int8),
+        't': np.array([1, 1], dtype=np.int8),
+        'a': np.array([1, 0], dtype=np.int8),
+        'b': np.array([1, 0], dtype=np.int8)
+    }
+
+    for i in range(data_size):
+        idx = i*sample_length
+
+        ecg_data[i] = record.p_signal[idx:idx+sample_length]
+        labels[i] = list(map(lambda x: dummy_encoder[x], label))[0]
+        
+    return (ecg_data, labels)
+
+
 def main() -> None:
     # Parse arguments
     args = parse_arguments(sys.argv[1:])
@@ -77,11 +149,29 @@ def main() -> None:
     if download:
         wfdb.dl_database("aftdb", ds_dir)
 
-    all_data = _list_all_data(ds_dir)
+    dataset_names = _list_all_data(ds_dir)
+    dataset_paths = [
+        os.path.join(ds_dir, dataset_dir)
+        for dataset_dir in dataset_names
+    ]
 
-    record = wfdb.rdrecord(f"dataset/learning-set/n01")
-    annotation = wfdb.rdann(f"dataset/learning-set/n01", extension="qrs")
-    print(record.p_signal.shape, annotation.sample)
+    all_records = [
+        (dataset_dir, sorted(_list_records(dataset_dir)))
+        for dataset_dir in dataset_paths
+    ]
+
+    records_and_annotations = [
+        (dataset_dir, _read_record_and_annotation(dataset_dir, record_name))
+        for dataset_dir, record_names in all_records
+        for record_name in record_names
+    ]
+
+    prepared_data = [
+        (dataset_path, _prepare_record_data(rec, ann, label))
+        for dataset_path, (rec, ann, label) in records_and_annotations
+    ]
+    
+    print((prepared_data[0]))
     return
 
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# python3 af-termination/neural-network/prepare-dataset.py --ds-dir dataset --sample-length 10 --sample-freq 10 --download
+python3 af-termination/neural-network/prepare-dataset.py --ds-dir dataset --sample-length 10 --sample-freq 10
